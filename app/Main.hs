18940285a054ab0cd8ece1df3fc4e70bea9429d7
rashadg1030 <rashad.sasaki@gmail.com>
2019-02-16 14:19:25 -0800

    Fixed color values for Chartreuse0, Chartreuse1, and Chartreuse2.

--- app/Main.hs
+++ app/Main.hs
@@ -35,9 +35,9 @@ tileMap sides (mx,my,click) = fromList $ do
   let color = Just $
         if (mx,my) == (x,y)
           -- Mouse color
-          then if click then Green1 else Spring1
+          then if click then Green1 else Green2
           -- Alternate background colors
-          else if (x + y) `mod` 2 == 0 then Green2 else Spring2
+          else if (x + y) `mod` 2 == 0 then Chartreuse1 else Chartreuse2
   let (symbol, shape) =
         if (x + y) `mod` 2 == 1
           then (Nothing, Nothing)
