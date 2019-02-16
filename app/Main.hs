dd99c877285286a5c746f4bc059b1cdb63ca6f59
rashadg1030 <rashad.sasaki@gmail.com>
2019-02-16 13:56:39 -0800

    Fixed color values for Azure0, Azure1, and Azure2. Azure0 may need to be darker.

--- app/Main.hs
+++ app/Main.hs
@@ -35,9 +35,9 @@ tileMap sides (mx,my,click) = fromList $ do
   let color = Just $
         if (mx,my) == (x,y)
           -- Mouse color
-          then if click then Green1 else Violet0
+          then if click then Green1 else Azure0
           -- Alternate background colors
-          else if (x + y) `mod` 2 == 0 then Violet1 else Violet2
+          else if (x + y) `mod` 2 == 0 then Azure1 else Azure2
   let (symbol, shape) =
         if (x + y) `mod` 2 == 1
           then (Nothing, Nothing)
